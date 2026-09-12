18e8a9e87ea144bc0f545ebe8cba3ad64edb119f
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2026-09-12 11:51:21 -0300

    Fix: no GetCycleCalendarDatesUseCase

--- app/src/main/java/com/example/ela/domain/usecase/cycle/GetCycleCalendarDatesUseCase.kt
+++ app/src/main/java/com/example/ela/domain/usecase/cycle/GetCycleCalendarDatesUseCase.kt
@@ -1,5 +1,7 @@
 package com.example.ela.domain.usecase.cycle
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.ela.domain.model.Cycle
 import com.example.ela.domain.model.CycleCalendarDates
 import com.example.ela.domain.model.CycleRecord
@@ -10,6 +12,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import kotlin.math.abs
 
+@RequiresApi(Build.VERSION_CODES.O)
 class GetCycleCalendarDatesUseCase @Inject constructor() {
 
     operator fun invoke(
